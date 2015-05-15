065fe72d995195d6dedcc34e0f8d664396494ec9
Ben Payne <ben.is.located@gmail.com>
2015-05-15 08:26:22 -0700

    update to arrows viz

--- bin/build_derivation_json.py
+++ bin/build_derivation_json.py
@@ -3,6 +3,7 @@
 # Ben Payne <ben.is.located@gmail.com>
 # builds the JSON for d3js
 # use: python bin/build_connections_JSON.py
+# based on "build_derivation_png.py"
 
 # files required as input: 
 #    lib_physics_graph.py
@@ -10,12 +11,11 @@
 #    inference_rules_database.xml
 #    expressions_database.xml
 # output: 
-#    out_no_labels.png
-#    out_with_labels.png
 
 # current bugs:
-# if an op is missing, i.e., "lib/images_infrule_png/subXforY.png" then it doesn't get built
 
+import re # regular expressions
+import subprocess
 import yaml # used to read "config.input"
 import os.path
 import sys
@@ -33,43 +33,28 @@ from xml.dom.minidom import parseString
 # https://yaml-online-parser.appspot.com/
 input_stream=file('config.input','r')
 input_data=yaml.load(input_stream)
-extension=input_data["file_extension_string"]
 makeAllGraphs=input_data["makeAllGraphs_boolean"] # set to false if you want to make just one graph; then you also need to set which graph to make
 if (not makeAllGraphs):
   name_of_set_to_make=input_data["name_of_set_to_make"]
-  filename_with_labels="graph_"+name_of_set_to_make+"_with_labels"
-  filename_without_labels="graph_"+name_of_set_to_make+"_with_labels"
+  filename="graph_"+name_of_set_to_make
 else: # making all graphs
-  filename_with_labels='graph_all_with_labels'
-  filename_without_labels='graph_all_without_labels'
-  
-make_graphviz=input_data["make_graphviz_boolean"]
-make_graphml=input_data["make_graphml_boolean"]
-make_networkx=input_data["make_networkx_boolean"]
+  filename='graph_all'
 
 
-eq_pictures=lib_path+'/images_statement_'+extension
-op_pictures=lib_path+'/images_infrule_'+extension
-feed_pictures=lib_path+'/images_feed_'+extension
+expression_pictures_path=lib_path+'/images_expression_png'
+infrule_pictures_path   =lib_path+'/images_infrule_png'
+feed_pictures_path      =lib_path+'/images_feed_png'
 
 connectionsDB    =physgraf.parse_XML_file(db_path+'/connections_database.xml')
 inference_rulesDB=physgraf.parse_XML_file(db_path+'/inference_rules_database.xml')
-statementsDB     =physgraf.parse_XML_file(db_path+'/expressions_database.xml')
+expressionsDB     =physgraf.parse_XML_file(db_path+'/expressions_database.xml')
 
-if (make_graphml):
-  graphml_file=open(output_path+'/connections_result.graphml','w')
-  physgraf.write_header_graphml(graphml_file)
+graphml_file=open(output_path+'/'+filename+'.json','w')
 
-if (make_networkx):
-  networkx_file=open(output_path+'/connections_result.py','w')
-  physgraf.write_header_networkx(networkx_file)
-
-if (make_graphviz):
-  graphviz_file=open(output_path+'/connections_result.gv','w')
-  physgraf.write_header_graphviz(graphviz_file)
 
 input_label_array=[]
 output_label_array=[]
+print("make all graphs:"+str(makeAllGraphs))
 
 # begin loop through connections
 for these_connections in connectionsDB.getElementsByTagName('connection_set'):
@@ -81,79 +66,118 @@ for these_connections in connectionsDB.getElementsByTagName('connection_set'):
     which_connection_set=name_of_set_to_make
     continue # skip this loop iteration
 
+  print("\nwhich_connection_set="+str(which_connection_set))
+
+  ary_of_unique_nodes=[] # [(infrule_name,infrule_tunid), (expression_punid,expression_tunid), (feed_tunid),...]
+  ary_of_edges=[] # [(infrule_tunid,expression_tunid),(infrule_tunid,expression_tunid),...] # pairs are source/target
+
   for connector in these_connections.getElementsByTagName('connection'):
     infrule_name=physgraf.convert_tag_to_content(connector,'infrule_name',0)
-    inference_rule_label=physgraf.convert_tag_to_content(connector,'infrule_tunid',0)
-    if (make_graphviz):
-      # http://www.graphviz.org/doc/info/attrs.html#d:image
-      #graphviz_file.write(inference_rule_label+" [shape=invtrapezium,color=red,image=\""+op_pictures+"/"+infrule_name+"."+extension+"\",labelloc=b,URL=\"http://google.com\"];\n")
-      graphviz_file.write(inference_rule_label+" [shape=invtrapezium,color=red,image=\""+op_pictures+"/"+infrule_name+"."+extension+"\"];\n")
-    if (make_graphml):
-      graphml_file.write('    <node id=\"'+inference_rule_label+'\"/>\n')
+    inference_rule_label=physgraf.convert_tag_to_content(connector,'infrule_temporary_unique_id',0)
+    # add each thing to list if not already present
+    templist=(infrule_name,inference_rule_label)
+    if templist not in ary_of_unique_nodes:
+      ary_of_unique_nodes.append(templist)
+
     for input_nodes in connector.getElementsByTagName('input'): # there may be multiple inputs for a given connection
 #       print('feed:')
 #       print connector.getElementsByTagName('input')[0].toxml()
-      for feed_counter in range(len(input_nodes.getElementsByTagName('feed_tunid'))):
-        feed_tunid=physgraf.convert_tag_to_content(input_nodes,'feed_tunid',feed_counter)
+      for feed_counter in range(len(input_nodes.getElementsByTagName('feed_temporary_unique_id'))):
+        feed_temporary_unique_id=physgraf.convert_tag_to_content(input_nodes,'feed_temporary_unique_id',feed_counter)
+        # add each thing to list if not already present
+        templist=(feed_temporary_unique_id)
+        if templist not in ary_of_unique_nodes:
+          ary_of_unique_nodes.append(templist)
+        edge_source_target=(feed_temporary_unique_id,inference_rule_label)
+#         print("FEED edge")
+#         print(edge_source_target)
+        ary_of_edges.append(edge_source_target)
 
-        if (make_graphviz):
-          graphviz_file.write(feed_tunid+" [shape=ellipse,color=red,image=\""+feed_pictures+"/"+feed_tunid+"."+extension+"\",labelloc=b,URL=\"http://feed.com\"];\n")
-          graphviz_file.write(feed_tunid+" -> "+inference_rule_label+"\n")
-        if (make_graphml):
-          graphml_file.write("    <node id=\""+feed_tunid+"\"/>\n")
-          graphml_file.write("    <edge source=\""+feed_tunid+"\" target=\""+inference_rule_label+"\"/>\n")
-        if (make_networkx):
-          networkx_file.write('G.add_edge(['+feed_tunid+','+inference_rule_label+'])\n')
       input_label_array=[]
-      for statement_counter in range(len(input_nodes.getElementsByTagName('statement_punid'))):
-        statement_indx=physgraf.convert_tag_to_content(input_nodes,'statement_punid',statement_counter)
-        input_label_array.append(statement_indx)
-        if (make_graphviz):
-          graphviz_file.write(statement_indx+" [shape=ellipse,color=red,image=\""+eq_pictures+"/"+statement_indx+"."+extension+"\",labelloc=b,URL=\"http://input.com\"];\n")
-          graphviz_file.write(statement_indx+" -> "+inference_rule_label+"\n")
-        if (make_graphml):  
-          graphml_file.write("    <node id=\""+statement_indx+"\"/>\n")
-          graphml_file.write("    <edge source=\""+statement_indx+"\" target=\""+inference_rule_label+"\"/>\n")
-        if (make_networkx):
-          networkx_file.write('G.add_edge(['+statement_indx+','+inference_rule_label+'])\n')
-
+      for statement_counter in range(len(input_nodes.getElementsByTagName('expression_permenant_unique_id'))):
+        expression_punid=physgraf.convert_tag_to_content(input_nodes,'expression_permenant_unique_id',statement_counter)
+        expression_tunid=physgraf.convert_tag_to_content(input_nodes,'expression_temporary_unique_id',statement_counter)
+        input_label_array.append(expression_punid)
+        # add each thing to list if not already present
+        templist=(expression_punid,expression_tunid)
+        if templist not in ary_of_unique_nodes:
+          ary_of_unique_nodes.append(templist)
+        edge_source_target=(expression_tunid,inference_rule_label)
+#         print("INPUT STATEMENT edge")
+#         print(edge_source_target)
+        ary_of_edges.append(edge_source_target)
+        
     for output_nodes in connector.getElementsByTagName('output'): # there may be multiple outputs for a given connection
-      for statement_counter in range(len(output_nodes.getElementsByTagName('statement_punid'))):
-        statement_indx=physgraf.convert_tag_to_content(output_nodes,'statement_punid',statement_counter)
-  
-        #  Given a "statement_indx" from connector, get the corresponding latex in statementDB
-        statement_latex=physgraf.convert_tpunid_to_latex(statement_indx,statementsDB,'statement')
-  
-        if (make_graphviz):
-          graphviz_file.write(statement_indx+" [shape=ellipse,color=red,image=\""+eq_pictures+"/"+statement_indx+"."+extension+"\",labelloc=b,URL=\"http://output.com\"];\n")
-          graphviz_file.write(inference_rule_label+" -> "+statement_indx+"\n")
-        if (make_graphml):
-          graphml_file.write("    <node id=\""+statement_indx+"\"/>\n")
-          graphml_file.write("    <edge source=\""+inference_rule_label+"\" target=\""+statement_indx+"\"/>\n")
-        if (make_networkx):
-          networkx_file.write('G.add_edge(['+inference_rule_label+','+statement_indx+'])\n')
-    # reset arrays to be empty for next connection
-    feed_array=[]
-    input_label_array=[]
-    output_label_array=[]
+      for statement_counter in range(len(output_nodes.getElementsByTagName('expression_permenant_unique_id'))):
+        expression_punid=physgraf.convert_tag_to_content(output_nodes,'expression_permenant_unique_id',statement_counter)
+        expression_tunid=physgraf.convert_tag_to_content(output_nodes,'expression_temporary_unique_id',statement_counter)
+
+        # add each thing to list if not already present
+        templist=(expression_punid,expression_tunid)
+        if templist not in ary_of_unique_nodes:
+          ary_of_unique_nodes.append(templist)
+        edge_source_target=(inference_rule_label,expression_tunid)
+#         print("OUTPUT STATEMENT edge")
+#         print(edge_source_target)
+        ary_of_edges.append(edge_source_target)
+
+  print("array of nodes:")
+  print(ary_of_unique_nodes)
+
+  print("array of edges:")
+  print(ary_of_edges)  
+
+  # replace tunid in edge array with array of node indices
+  ary_of_edge_indices=[]
+  for edge_pair in ary_of_edges:
+    source_tunid=edge_pair[0]
+    target_tunid=edge_pair[1]
+    for index,node in enumerate(ary_of_unique_nodes):
+      if len(node)==2:  # list, either infrule or expression
+        if node[1]==source_tunid:
+          source_index=index
+        if node[1]==target_tunid:
+          target_index=index
+      else: # feed tunid
+        if node==source_tunid:
+          source_index=index
+        if node==target_tunid:
+          target_index=index
+    templist=(source_index,target_index)
+    ary_of_edge_indices.append(templist)
+
+  print("array of edge indices:")
+  print(ary_of_edge_indices)
+
+  # determine image dimensions
+  for node in ary_of_unique_nodes:
+    if len(node)==2:  # list, either infrule or expression
+      if node[0].isdigit(): # expression     
+        output = subprocess.check_output("file "+expression_pictures_path+"/"+node[0]+".png", shell=True)
+        matchObj=re.match( r'.*data, (\d+) x (\d+), .*', output, re.M|re.I)
+        img_width=matchObj.group(1)
+        img_height=matchObj.group(2)
+        print("  {\"img\": \"http://allofphysicsgraph.github.io/proofofconcept/lib/images_expression_png/"+node[0]+".png\", \"width\": "+img_width+", \"height\": "+img_width+", \"label\": \""+node[0]+"|"+node[1]+"\"},")
+      else: # infrule
+#         print(node[0])
+        output = subprocess.check_output("file "+infrule_pictures_path+"/"+node[0]+".png", shell=True)
+#         print(output)
+        matchObj=re.match( r'.*data, (\d+) x (\d+), .*', output, re.M|re.I)
+        img_width=matchObj.group(1)
+        img_height=matchObj.group(2)
+        print("  {\"img\": \"http://allofphysicsgraph.github.io/proofofconcept/lib/images_infrule_png/"+node[0]+".png\", \"width\": "+img_width+", \"height\": "+img_width+", \"label\": \""+node[0]+"|"+node[1]+"\"},")
+    else: # feed
+      output = subprocess.check_output("file "+feed_pictures_path+"/"+node+".png", shell=True)
+      matchObj=re.match( r'.*data, (\d+) x (\d+), .*', output, re.M|re.I)
+      img_width=matchObj.group(1)
+      img_height=matchObj.group(2)
+      print("  {\"img\": \"http://allofphysicsgraph.github.io/proofofconcept/lib/images_expression_png/"+node+".png\", \"width\": "+img_width+", \"height\": "+img_width+", \"label\": \""+node+"\"},")
+
 
 # end loop through connections
 
-if (make_graphml):
-  graphml_file.write('  </graph>\n</graphml>\n')
-  graphml_file.close()
 
-if (make_networkx):
-  networkx_file.write('nx.plot()\n')
-  networkx_file.write('plt.show()\n')
-  networkx_file.close()
 
-if (make_graphviz):
-  physgraf.write_footer_graphviz(graphviz_file)
-  graphviz_file.close()
-  physgraf.convert_graphviz_to_pictures(extension,\
-     output_path,makeAllGraphs,which_connection_set,\
-     filename_with_labels,filename_without_labels)
 
 sys.exit("done")
 # end of file 
