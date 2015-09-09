a9cd93208681e300b80b159365ae1fc906ce9515
Ben Payne <ben.is.located@gmail.com>
2015-09-08 21:18:38 -0400

    updated PDFs on site

---
+++ site/documentation_PDFs/grab_current_copy_from_git.sh
@@ -0,0 +1,5 @@
+cp ../../doc/physics_graph_motivation.pdf .
+cp ../../doc/introduction/introduction.pdf .
+cp ../../doc/database_comparison/database_comparison.pdf .
+cp ../../doc/syntax_comparison/syntax_comparison.pdf .
+cp ../../doc/user_manual/user_manual.pdf .
