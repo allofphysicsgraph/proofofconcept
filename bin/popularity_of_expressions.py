8a00e50124a335a1061d6bee54590b8acad2132f
Ben Payne <ben.is.located@gmail.com>
2015-11-02 23:11:11 -0500

    now with popularity

--- bin/popularity_of_expressions.py
+++ bin/popularity_of_expressions.py
@@ -23,9 +23,12 @@ connectionsDB=   input_data["connectionsDB_path"]
 expressionsDB=   input_data["expressionsDB_path"]
 
 connections_list_of_dics=physgraf.convert_connections_csv_to_list_of_dics(connectionsDB)
+
 [connection_feeds,connection_expr_perm,connection_expr_temp,\
 connection_infrules,connection_infrule_temp]=\
 physgraf.separate_connection_lists(connections_list_of_dics)
+
+expressions_list_of_dics=physgraf.convert_expressions_csv_to_list_of_dics(expressionsDB)
 
 #http://stackoverflow.com/questions/2870466/python-histogram-one-liner
 hist = {}
@@ -33,6 +36,9 @@ for x in connection_expr_perm: hist[x] = hist.pop(x,0) + 1
 
 #http://stackoverflow.com/questions/613183/python-sort-a-dictionary-by-value
 for w in sorted(hist, key=hist.get, reverse=True):
-  print (str(hist[w])+" "+w+" "+expressionsDB[w])
-  
+  for this_expr_dic in expressions_list_of_dics:
+    if (this_expr_dic['permanent index']==w):
+      break
+  print (str(hist[w])+" "+w+" "+this_expr_dic['expression latex'])
+    
 sys.exit("done")
--- bin/popularity_of_inference_rules.py
+++ bin/popularity_of_inference_rules.py
@@ -9,6 +9,7 @@
 
 # current bugs:
 
+import yaml
 import sys
 import os
 lib_path = os.path.abspath('lib')
@@ -16,17 +17,22 @@ sys.path.append(lib_path) # this has to proceed use of physgraph
 db_path = os.path.abspath('databases')
 import lib_physics_graph as physgraf
 
+# https://yaml-online-parser.appspot.com/
+input_stream=file('config.input','r')
+input_data=yaml.load(input_stream)
 connectionsDB=   input_data["connectionsDB_path"]
 connections_list_of_dics=physgraf.convert_connections_csv_to_list_of_dics(connectionsDB)
 
-
+[connection_feeds,connection_expr_perm,connection_expr_temp,\
+connection_infrules,connection_infrule_temp]=\
+physgraf.separate_connection_lists(connections_list_of_dics)
 
 #http://stackoverflow.com/questions/2870466/python-histogram-one-liner
 hist = {}
-for x in rule_ary: hist[x] = hist.pop(x,0) + 1  
+for x in connection_infrules: hist[x] = hist.pop(x,0) + 1  
 
 #http://stackoverflow.com/questions/613183/python-sort-a-dictionary-by-value
 for w in sorted(hist, key=hist.get, reverse=True):
-  print (str(hist[w])+" "+w)
+  print (str(hist[w])+" "+w+" ")
   
 sys.exit("done")
