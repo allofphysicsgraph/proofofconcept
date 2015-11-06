34c695a4eea3ffa911c08e4366510b6c4e824cd3
Ben Payne <ben.is.located@gmail.com>
2015-11-05 21:48:18 -0500

    testing

--- bin/create_d3js_html_per_derivation.sh
+++ bin/create_d3js_html_per_derivation.sh
@@ -1,17 +1,26 @@
+#!/bin/bash
 # prints the commands for running each and all the derivations
 
-cut -f 1 -d ',' databases/connections_database.csv | uniq > output/list_of_derivations.dat
+if [ "$#" -ne 2 ]; then
+    echo -e "\nrequires 2 arguments:"
+    echo -e "output path (ending in /)"
+    echo -e "name of json file (no spaces, no extension)\n"
+    exit
+fi
 
-while read line; do 
-  this_deriv=`echo $line | sed -e "s/ /_/g" | sed -e "s/\"//g"` # replace space with underscore, remove "
-  file_name=output/${this_deriv}.html
-  cp lib/d3js_part1_head.html ${file_name}
-  echo -e "d3.json(\"http://allofphysicsgraph.github.io/proofofconcept/site/json/generated_from_project/"${this_deriv}".json\", function(error, root) {" >> ${file_name}
-  cat lib/d3js_part2_tail.html >> ${file_name}
+# $1 = output path
+# $2 = name of json file, no spaces, no extension
+
+site_dir=testing
+# site_dir=generated_from_project # actual PDG content
+
+file_name=$1/$2.html
+cp lib/d3js_part1_head.html ${file_name}
+echo -e "d3.json(\"http://allofphysicsgraph.github.io/proofofconcept/site/json/"${site_dir}"/"$2".json\", function(error, root) {" >> ${file_name}
+cat lib/d3js_part2_tail.html >> ${file_name}
   
-  echo -e "<P>Picture from Graphviz:</P>" >> ${file_name}
-  echo -e "<P><img src=\"http://allofphysicsgraph.github.io/proofofconcept/site/pictures/generated_from_project/graph_"${this_deriv}"_without_labels.png\" width=\"800\"></P>" >> ${file_name}
-  echo -e "</body>" >> ${file_name}
-  echo -e "</html>" >> ${file_name}
+echo -e "<P>Picture from Graphviz:</P>" >> ${file_name}
+echo -e "<P><img src=\"http://allofphysicsgraph.github.io/proofofconcept/site/pictures/"${site_dir}"/graph_"$2"_without_labels.png\" width=\"800\"></P>" >> ${file_name}
+echo -e "</body>" >> ${file_name}
+echo -e "</html>" >> ${file_name}
   
-done < output/list_of_derivations.dat
--- bin/list_derivations.sh
+++ bin/list_derivations.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+echo -e "==== as is: ===="
 cut -f 1 -d ',' databases/connections_database.csv | uniq
 
+echo -e "==== with underscore: ===="
+# replace space with underscore, remove "
+cut -f 1 -d ',' databases/connections_database.csv | uniq | sed -e "s/ /_/g" | sed -e "s/\"//g" 
