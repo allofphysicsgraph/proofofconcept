76f0cf92d814eb3d800feb2fe6cc844c7d3faa70
Ben Payne <ben.is.located@gmail.com>
2015-11-30 16:24:21 -0500

    works

--- sandbox/flask/multi_page_form/controller.py
+++ sandbox/flask/multi_page_form/controller.py
@@ -1,7 +1,7 @@
 # https://github.com/hplgit/web4sciapps/blob/master/doc/src/web4sa/src-web4sa/apps/flask_apps/vib1/controller.py
 
 from model import SelectInfRule,InfRuleArguments,AddAnotherStep
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from compute import compute
 
 app = Flask(__name__)
@@ -12,16 +12,23 @@ def index():
     if request.method == 'POST' and form.validate():
         print form.addStep.data
         if (form.addStep.data=='y'):
-            return render_template('choose_inf_rule_for_this_step.html')
-        else:         
-            return render_template('view_current_derivation.html', form=form)
-    else:
-        return render_template('view_current_derivation.html', form=form)
+            return redirect(url_for('choose_inf_rule'))
+#             return render_template('choose_inf_rule_for_this_step.html')
+    return render_template('view_current_derivation.html', form=form)
 
 @app.route('/choose_inf_rule_for_this_step.html', methods=['GET', 'POST'])
 def choose_inf_rule():
     form = SelectInfRule(request.form)
+    if request.method == 'POST' and form.validate():
+        return redirect(url_for('provide_arguments_for_this_infrule'))
     return render_template('choose_inf_rule_for_this_step.html', form=form)
+    
+@app.route('/provide_arguments_for_this_infrule.html', methods=['GET', 'POST'])
+def provide_arguments_for_this_infrule():
+    form = InfRuleArguments(request.form)
+    if request.method == 'POST' and form.validate():
+        return redirect(url_for('index'))
+    return render_template('provide_arguments_for_this_infrule.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
