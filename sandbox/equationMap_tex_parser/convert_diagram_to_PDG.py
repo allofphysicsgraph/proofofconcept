0752747ccb169d5f643f0b4947467873949eb937
Ben Payne <ben.is.located@gmail.com>
2015-12-07 10:51:59 -0500

    separates infrule from latex

--- sandbox/equationMap_tex_parser/convert_diagram_to_PDG.py
+++ sandbox/equationMap_tex_parser/convert_diagram_to_PDG.py
@@ -43,17 +43,37 @@ trimmed_list_of_lines=list_of_lines[2:len(list_of_lines)-3]
 #     print(this_line)
 
 list_of_expressions=[]
+list_of_infrules=[]
 for line_indx,this_line in enumerate(trimmed_list_of_lines):
     if (this_line=="\\begin{equation}"):
+        expr_dic={}
         search_object=re.search(r'[0-9]{4}',trimmed_list_of_lines[line_indx+2])
         node_ID=search_object.group()
 
-        expr_dic={}
-        expr_dic['latex']=trimmed_list_of_lines[line_indx+1]        
-        expr_dic['id']=node_ID
-        list_of_expressions.append(expr_dic)
+        this_line_is_expr=True
+        for list_indx,this_node in enumerate(list_of_nodes):
+            if (node_ID==this_node['id'] and this_node['type']=='text'): # node is infrule, not expression
+                this_line_is_expr=False
+                infrule_dic={}
+                infrule_dic['infrule']=trimmed_list_of_lines[line_indx+1]        
+                infrule_dic['temp id']=node_ID
+                list_of_infrules.append(infrule_dic)        
+        if (this_line_is_expr):
+            expr_dic['latex']=trimmed_list_of_lines[line_indx+1]        
+            expr_dic['id']=node_ID
+            list_of_expressions.append(expr_dic)
 
 print("number of expressions found: "+str(len(list_of_expressions)))
 for this_expr in list_of_expressions:
     print(this_expr)
+print("number of infrules found: "+str(len(list_of_infrules)))
+for this_infrule in list_of_infrules:
+    print(this_infrule)
+
+# sanity check
+if ( len(list_of_expressions)+len(list_of_infrules) != len(list_of_nodes) ):
+    print("ERROR: number of nodes does not equal sum of infrule and expr")
+    print(len(list_of_expressions))
+    print(len(list_of_infrules))
+    print(len(list_of_nodes))
 
