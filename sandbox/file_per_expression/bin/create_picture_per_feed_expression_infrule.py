d9b0a24fb6cf16b7e73754cb0db05b8b298d48b5
Ben Payne <ben.is.located@gmail.com>
2016-01-01 21:49:40 -0600

    new pics

--- sandbox/file_per_expression/bin/create_picture_per_feed_expression_infrule.py
+++ sandbox/file_per_expression/bin/create_picture_per_feed_expression_infrule.py
@@ -2,8 +2,8 @@
 # Physics Equation Graph
 # Ben Payne <ben.is.located@gmail.com>
 
-import sys.path
-import os.path
+import sys
+import os
 lib_path = os.path.abspath('/Users/benpayne/version_controlled/proofofconcept/lib/')
 sys.path.append(lib_path) # this has to proceed use of physgraph
 import lib_physics_graph as physgraf
@@ -11,9 +11,9 @@ import lib_physics_graph as physgraf
 from os import listdir
 from os.path import isfile, join
 
-mypath='../feeds'
+# mypath='../feeds'
 # mypath='../expressions'
-# mypath='../inference_rules'
+mypath='../inference_rules'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 # print onlyfiles
 extension='png'
@@ -30,3 +30,8 @@ for this_filename in onlyfiles:
         latex_expression="$"+read_data.rstrip()+"$"
         physgraf.make_picture_from_latex_expression(filename,mypath,latex_expression,extension)
 
+os.remove('tmp.aux')
+os.remove('tmp.dvi')
+os.remove('tmp.out')
+os.remove('tmp.log')
+os.remove('tmp.tex')
