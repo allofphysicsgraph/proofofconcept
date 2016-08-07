65741c663f1bbbf4acc48e2112277954b8c8d52a
Mike <Mike@lcd-outlet.com>
2016-08-06 22:28:37 -0700

    autopep8

--- v3_CSV/bin/popularity_of_expressions.py
+++ v3_CSV/bin/popularity_of_expressions.py
@@ -1,11 +1,11 @@
 #!/opt/local/bin/python
 # Physics Equation Graph
 # Ben Payne <ben.is.located@gmail.com>
-# 
-# 
+#
+#
 # use: python sandbox/list_connection_sets.py
-# input: 
-# output: 
+# input:
+# output:
 
 # current bugs:
 
@@ -13,32 +13,35 @@ import yaml        # for reading "config.input"
 import sys
 import os
 lib_path = os.path.abspath('../lib')
-sys.path.append(lib_path) # this has to proceed use of physgraph
+sys.path.append(lib_path)  # this has to proceed use of physgraph
 import lib_physics_graph as physgraf
 
 # https://yaml-online-parser.appspot.com/
-input_stream=file('config.input','r')
-input_data=yaml.load(input_stream)
-connectionsDB=   input_data["connectionsDB_path"]
-expressionsDB=   input_data["expressionsDB_path"]
+input_stream = file('config.input', 'r')
+input_data = yaml.load(input_stream)
+connectionsDB = input_data["connectionsDB_path"]
+expressionsDB = input_data["expressionsDB_path"]
 
-connections_list_of_dics=physgraf.convert_connections_csv_to_list_of_dics(connectionsDB)
+connections_list_of_dics = physgraf.convert_connections_csv_to_list_of_dics(
+    connectionsDB)
 
-[connection_feeds,connection_expr_perm,connection_expr_temp,\
-connection_infrules,connection_infrule_temp]=\
-physgraf.separate_connection_lists(connections_list_of_dics)
+[connection_feeds, connection_expr_perm, connection_expr_temp,
+ connection_infrules, connection_infrule_temp] =\
+    physgraf.separate_connection_lists(connections_list_of_dics)
 
-expressions_list_of_dics=physgraf.convert_expressions_csv_to_list_of_dics(expressionsDB)
+expressions_list_of_dics = physgraf.convert_expressions_csv_to_list_of_dics(
+    expressionsDB)
 
-#http://stackoverflow.com/questions/2870466/python-histogram-one-liner
+# http://stackoverflow.com/questions/2870466/python-histogram-one-liner
 hist = {}
-for x in connection_expr_perm: hist[x] = hist.pop(x,0) + 1 
+for x in connection_expr_perm:
+    hist[x] = hist.pop(x, 0) + 1
 
-#http://stackoverflow.com/questions/613183/python-sort-a-dictionary-by-value
+# http://stackoverflow.com/questions/613183/python-sort-a-dictionary-by-value
 for w in sorted(hist, key=hist.get, reverse=True):
-  for this_expr_dic in expressions_list_of_dics:
-    if (this_expr_dic['permanent index']==w):
-      break
-  print (str(hist[w])+" "+w+" "+this_expr_dic['expression latex'])
-    
+    for this_expr_dic in expressions_list_of_dics:
+        if (this_expr_dic['permanent index'] == w):
+            break
+    print (str(hist[w]) + " " + w + " " + this_expr_dic['expression latex'])
+
 sys.exit("done")
