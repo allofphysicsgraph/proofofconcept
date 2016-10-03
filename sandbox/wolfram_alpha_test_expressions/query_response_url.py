26b0d9fc8c49ede12c897b4bf4cd050765747a81
mike <mike@lcd-outlet.com>
2016-10-03 22:05:33 +0000

    Wolfram Queries

--- sandbox/wolfram_alpha_test_expressions/query_response_url.py
+++ sandbox/wolfram_alpha_test_expressions/query_response_url.py
@@ -1,18 +1,11 @@
-import urllib
-import urllib.request
-import xml.etree.ElementTree as ET # http://stackoverflow.com/questions/15442130/python-xml-parsing-for-wolfram-api
+import re
+import  requests
+expression=raw_input("input expression eg What time is it in London? : ")
+print expression
+data=requests.get("http://api.wolframalpha.com/v2/query?input={}&appid=APLTT9-9WG78GYE65".format(expression)).text
+for line in  re.findall('plaintext>(.*?)</plaintext>\n',data):
+	print line
 
-f=open('wolfram_app_id','r')
-appid=f.read()
-f.close()
-appid=appid.rstrip() # http://effbot.org/pyfaq/is-there-an-equivalent-to-perl-s-chomp-for-removing-trailing-newlines-from-strings.htm
 
-xml_data=urllib.request.urlopen("http://api.wolframalpha.com/v2/query?input=sqrt+2&appid=APLTT9-9WG78GYE65").read()
-root = ET.fromstring(xml_data)
 
-for pod in root.findall('.//pod'):
-    print(pod.attrib['title'])
-    for pt in pod.findall('.//plaintext'):
-        if pt.text:
-            print('-', pt.text)
 
