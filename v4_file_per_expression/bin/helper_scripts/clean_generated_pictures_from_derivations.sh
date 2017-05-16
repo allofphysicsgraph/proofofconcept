b247efc8b9212218dd65a38853cf303fcab14319
PelicanHPC user <mike@lcd-outlet.com>
2017-05-16 20:21:36 +0000

    updating branch

---
+++ v4_file_per_expression/bin/helper_scripts/clean_generated_pictures_from_derivations.sh
@@ -0,0 +1,2 @@
+find ../derivations -name "*.png" -type f -delete
+find ../identities  -name "*.png" -type f -delete
---
+++ v4_file_per_expression/bin/helper_scripts/generate_new_random_index.sh
@@ -0,0 +1,31 @@
+# Ben Payne <ben.is.located@gmail.com>
+# Physics Derivation Graph
+# 20160129
+
+rm temp_id.dat
+find ../derivations -depth 1 -type d | while read this_dir; do
+    #echo "looking for temporary IDs in ${this_dir}"
+    cat "${this_dir}/derivation_edge_list.csv" | tr ',' '\n' >> temp_id.dat # convert from two column to single column
+done
+# at this point we have a large single column list of temporary IDs
+cat temp_id.dat | sort | uniq > temp_id2.dat
+mv temp_id2.dat temp_id.dat
+
+#need a 7 digit random number
+current_guess=`head -c7 <(echo $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM)`
+if grep -Fxq $current_guess temp_id.dat; then echo "already in file; try again"; else echo "temp ID not in use: $current_guess"; fi
+
+
+rm expr_id.dat
+find ../derivations -depth 1 -type d | while read this_dir; do
+    #echo "looking for expression IDs in ${this_dir}"
+    cat "${this_dir}/expression_identifiers.csv" | cut -d',' -f2 >> expr_id.dat # convert from two column to single column
+done
+# at this point we have a large single column list of permanent IDs
+cat expr_id.dat | sort | uniq > expr_id2.dat
+mv expr_id2.dat expr_id.dat
+
+# generate a new random index, then check that it doesn't already exist
+# need a 10 digit random number
+current_guess=`head -c10 <(echo $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM)`
+if grep -Fxq $current_guess expr_id.dat; then echo "already in file; try again"; else echo "perm ID not in use: $current_guess"; fi
