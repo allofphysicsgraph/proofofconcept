caf5d8c25f08c27a8fcdfd7272a8296da75f4b8e
mike <mike@lcd-outlet.com>
2017-11-11 18:14:13 +0000

    installer

--- debian_installer.sh
+++ debian_installer.sh
@@ -3,3 +3,5 @@ sudo apt-get install python-pip
 sudo pip install -U pip
 sudo pip install pyyaml
 sudo apt-get install haskell-platform
+wget http://isabelle.in.tum.de/dist/Isabelle2017_app.tar.gz
+tar -zxvf Isabelle2017_app.tar.gz
