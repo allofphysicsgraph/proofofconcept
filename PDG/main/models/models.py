1f41c0eacf762f5bb1911c6ba4a7acd07a7ee0d5
mike <mike@lcd-outlet.com>
2018-05-05 12:25:25 -0400

    added csv models

--- PDG/main/models/models.py
+++ PDG/main/models/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class InferenceRules(models.Model):
-    inference_rule_name = models.CharField(max_length=128, null=True)
+    inference_rule_name = models.CharField(null=True)
     number_of_arguments = models.IntegerField(null=True)
     number_of_feeds = models.IntegerField(null=True)
     number_of_input_expressions = models.IntegerField(null=True)
@@ -12,4 +12,34 @@ class InferenceRules(models.Model):
     comments = models.CharField(max_length=128, null=True)
     latex_expression = models.CharField(max_length=128, null=True)
 
+
+class Feeds(models.Model):
+    index = models.IntegerField(null=True)
+    latex = models.CharField(max_length=128, null=True)
+
+
+class Expressions(models.Model):
+    index = models.IntegerField(null=True)
+    latex = models.CharField(max_length=128, null=True)
+
+
+class Connections(models.Model):
+    derivation_name = models.IntegerField(null=True)
+    step_index = models.CharField(max_length=128, null=True)
+    input_type = models.IntegerField(null=True)
+    input_temporary_index = models.CharField(max_length=128, null=True)
+    input_permanent_index = models.IntegerField(null=True)
+    output_type = models.CharField(max_length=128, null=True)
+    output_temporary_index = models.IntegerField(null=True)
+    output_permanent_index = models.CharField(max_length=128, null=True)
+
+
+class Symbols(models.Model):
+    unique_id = models.IntegerField(null=True)
+    symbol = models.CharField(null=True)
+    type = models.CharField(null=True)
+    value = models.FloatField(null=True)
+    units = models.CharField(null=True)
+    description = models.CharField(null=True)
+    cas_sympy = models.CharField(null=True)
 
