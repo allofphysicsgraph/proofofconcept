8b6648fc7960d3f718b2aea57696e70b61ea055a
mike <mike@lcd-outlet.com>
2018-05-05 11:58:04 -0400

    removed max length from integer field

--- PDG/main/models/models.py
+++ PDG/main/models/models.py
@@ -5,9 +5,11 @@ from django.db import models
 
 class InferenceRules(models.Model):
     inference_rule_name = models.CharField(max_length=128, null=True)
-    number_of_arguments = models.IntegerField(max_length=128, null=True)
+    number_of_arguments = models.IntegerField(null=True)
     number_of_feeds = models.IntegerField(null=True)
     number_of_input_expressions = models.IntegerField(null=True)
     number_of_output_expressions = models.IntegerField(null=True)
     comments = models.CharField(max_length=128, null=True)
     latex_expression = models.CharField(max_length=128, null=True)
+
+
