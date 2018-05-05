a43f10bf3ed16825223fee89b7ace5d60ec51e24
mike <mike@lcd-outlet.com>
2018-05-05 15:13:41 -0400

    forms for all models

--- PDG/PDG/urls.py
+++ PDG/PDG/urls.py
@@ -17,9 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 
 
-from main.views import HomeView,NewInferenceRule
+from main.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',HomeView.as_view(),name="home"),
-    url(r'^NewInferenceRule$',NewInferenceRule,name="NewInferenceRule"),
-]
+    url(r'^add_inference_rule$',add_inference_rule,name="add_inference_rule"),
+    url(r'^add_feed$',add_feed,name="add_feed"),
+    url(r'^add_expression$',add_expression,name="add_expression"),
+    url(r'^add_connection$',add_connection,name="add_connection"),
+    url(r'^add_symbol$',add_symbol,name="add_symbol"),
+    ]
--- PDG/main/views/views.py
+++ PDG/main/views/views.py
@@ -4,9 +4,29 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from main.forms import InferenceRuleForm
+from main.forms import *
 
 
-def NewInferenceRule(request):
-    form = InferenceRuleForm()
-    return render(request, 'index.html', {'form': form})
+def add_inference_rule(request):
+    inference_rule_form = InferenceRuleForm()
+    return render(request, 'index.html', {'inference_rule_form': inference_rule_form})
+
+
+def add_feed(request):
+    feed_form = FeedForm()
+    return render(request, 'index.html', {'feed_form': feed_form})
+
+
+def add_expression(request):
+    expression_form = ExpressionForm()
+    return render(request, 'index.html', {'expression_form': expression_form})
+
+
+def add_connection(request):
+    connection_form = ConnectionForm()
+    return render(request, 'index.html', {'connection_form': connection_form})
+
+
+def add_symbol(request):
+    symbol_form = SymbolForm()
+    return render(request, 'index.html', {'symbol_form': symbol_form})
