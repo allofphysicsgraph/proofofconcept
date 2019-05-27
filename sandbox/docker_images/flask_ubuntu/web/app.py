5be0d6067ee81f36b8364e821661f49f7b073b04
Ben Payne <ben.is.located@gmail.com>
2019-05-27 08:40:42 -0400

    docker attempts in Ubuntu

---
+++ sandbox/docker_images/flask_ubuntu/web/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+app = Flask(__name__)
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+@app.route('/')
+def hello_whale():
+    return render_template("hello.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
