dc2d95f7ef1a5015c4b28220ecb5e9c5a20603b3
Ben Payne <ben.is.located@gmail.com>
2019-06-26 22:04:31 -0400

    workflow established

--- sandbox/docker_images/flask_ubuntu/web/controller.py
+++ sandbox/docker_images/flask_ubuntu/web/controller.py
@@ -6,13 +6,15 @@ import compute
 
 app = Flask(__name__, static_folder='static')
 
-class InputForm(Form):
+class EquationInputForm(Form):
 #    r = FloatField(validators=[validators.InputRequired()])
 #    r = FloatField()
     latex = StringField(validators=[validators.InputRequired()])
 
+class NameOfDerivationInputForm(Form):
+    name_of_derivation = StringField(validators=[validators.InputRequired()])
 
-# prevent cached responses; see https://stackoverflow.com/questions/47376744/how-to-prevent-cached-response-flask-server-using-chrome
+# goal = prevent cached responses; see https://stackoverflow.com/questions/47376744/how-to-prevent-cached-response-flask-server-using-chrome
 # The following doesn't work; F12 > Network > Disable cache
 @app.after_request
 def add_header(r):
@@ -29,7 +31,67 @@ def add_header(r):
 # View
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = InputForm(request.form)
+    """ 
+    index.html contains hyperlinks to pages like:
+    * start new derivation
+    * edit existing derivation
+    * edit inference rule
+    * view existing derivations
+    """
+    return render_template('index.html')
+
+@app.route('/start_new_derivation/', methods=['GET', 'POST'])
+def start_new_derivation():
+    form = NameOfDerivationInputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name_of_derivation = form.name_of_derivation.data
+        print(name_of_derivation)
+        return render_template("select_inference_rule.html")
+    else:
+        return render_template("start_new_derivation.html",form=form)
+
+@app.route('/edit_existing_derivation', methods=['GET', 'POST'])
+def edit_existing_derivation():
+    return render_template("edit_existing_derivation.html")
+
+@app.route('/edit_inference_rule', methods=['GET', 'POST'])
+def edit_inference_rule():
+    return render_template("edit_inference_rule.html")
+
+@app.route('/list_all_inference_rules', methods=['GET', 'POST'])
+def list_all_inference_rules():
+    return render_template("list_all_inference_rules.html")
+
+@app.route('/select_derivation_to_edit', methods=['GET', 'POST'])
+def select_derivation_to_edit():
+    return render_template("select_derivation_to_edit.html")
+
+@app.route('/select_derivation_step_to_edit', methods=['GET', 'POST'])
+def select_derivation_step_to_edit():
+    return render_template("select_derivation_step_to_edit.html")
+
+@app.route('/list_all_expressions', methods=['GET', 'POST'])
+def list_all_expressions():
+    return render_template("list_all_expressions.html")
+
+
+@app.route('/view_existing_derivations', methods=['GET', 'POST'])
+def view_existing_derivations():
+    return render_template("view_existing_derivations.html")
+
+@app.route('/select_inference_rule/', methods=['GET', 'POST'])
+def select_inference_rule():
+    """
+    rather than a local list, 
+    this function should poll the SQL database
+    """
+    list_of_inf_rules = ['add X to both sides','multiply both sides by X']
+
+    return render_template("select_inference_rule.html",inf_rule_list=list_of_inf_rules)
+
+@app.route('/enter_equation/', methods=['GET', 'POST'])
+def create_eq_as_png():
+    form = EquationInputForm(request.form)
     if request.method == 'POST' and form.validate():
         latex_as_str = form.latex.data
         #latex_stdout,latex_stderr,png_stdout,png_stderr,name_of_png = compute_latex(latex_as_str,print_debug)
