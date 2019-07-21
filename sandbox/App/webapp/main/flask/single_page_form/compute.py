8d8711f743ccd5ceffbb8fa09d48c38c58889b4e
mike <mike@lcd-outlet.com>
2019-07-22 01:44:45 +0200

    remove Django, reset branch structure to match gh-pages

--- sandbox/App/webapp/main/flask/single_page_form/compute.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def compute(input_expression,inference_rule,feed,output_expression):
-
-    return random.randint(100,999),random.randint(100,999)
-
-if __name__ == '__main__':
-    print compute(1, 0.1) # default values
-
