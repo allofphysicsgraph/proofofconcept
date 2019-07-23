5e560080fd52da6e81406dd8cafe1fc79b8759c6
mike <mike@lcd-outlet.com>
2019-07-23 11:24:49 +0200

    package requirements

--- Installers/debian_installer.sh
+++ Installers/debian_installer.sh
@@ -1,5 +1,6 @@
 sudo apt-get install graphviz
-sudo apt-get install python-pip
-sudo pip install -U pip
-sudo pip install pyyaml
-
+sudo apt-get install python3-pip
+sudo pip3 install -U pip3
+virtualenv venv -p python 3.6
+source venv/bin/activate
+pip install -r requirements_py36_deb.txt 
