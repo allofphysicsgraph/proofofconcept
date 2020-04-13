dd5fbbcde4439342e175af3b8b3b1a955752f273
Ben Payne <ben.is.located@gmail.com>
2020-04-13 15:06:17 -0400

    before switching

--- count_lines.sh
+++ count_lines.sh
@@ -11,12 +11,15 @@ count_c_files=`find . -type f | sed '/\.\/\./d' | awk -F'.' '{print $NF}' | sort
 count_csv_files=`find . -type f | sed '/\.\/\./d' | awk -F'.' '{print $NF}' | sort | uniq -c | grep " csv$" | sed 's/\(.*\)csv/\1/'`
 count_png_files=`find . -type f | sed '/\.\/\./d' | awk -F'.' '{print $NF}' | sort | uniq -c | grep " png$" | sed 's/\(.*\)png/\1/'`
 count_html_files=`find . -type f | sed '/\.\/\./d' | awk -F'.' '{print $NF}' | sort | uniq -c | grep " html$" | sed 's/\(.*\)html/\1/'`
+count_sh_file=`find . -type f | sed '/\.\/\./d' | awk -F'.' '{print $NF}' | sort | uniq -c | grep " sh$" | sed 's/\(.*\)sh/\1/'`
 
-echo "`date +%s` ${count_py_files} ${count_c_files} ${count_csv_files} ${count_png_files} ${count_html_files}" >> record_of_file_counts.log
+echo "{'date':`date +%s`, 'python file count':${count_py_files}, 'c file count':${count_c_files},'csv file count':${count_csv_files}, 'png file count':${count_png_files}, 'html file count':${count_html_files}, 'sh file count':${count_sh_file}}" >> record_of_file_counts.log
 
 # count lines of code
 py_line_count=`find . -name "*.py" -type f -exec grep . {} \; | sed -n '/^# /!p' | wc -l`
 tex_line_count=`find . -name "*.tex" -type f -exec grep . {} \; | sed -n '/^% /!p' | wc -l`
 html_line_count=`find . -name "*.html" -type f -exec grep . {} \; | wc -l`
+sh_line_count=`find . -name "*.sh" -type f -exec grep . {} \; | wc -l`
 
-echo "`date +%s` ${py_line_count} ${tex_line_count} ${html_line_count}" >> record_of_line_counts.log
+
+echo "{'date':`date +%s`, 'python line count':${py_line_count}, 'tex line count':${tex_line_count}, 'html line count':${html_line_count}, 'sh line count':${sh_line_count}}" >> record_of_line_counts.log
